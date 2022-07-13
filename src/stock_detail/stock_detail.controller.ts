47df240000741edcdf294abc7cfecd792dec92c7
Aman Chauhan <amanschauhan7@gmail.com>
2022-07-13 13:08:30 +0530

    Migrated Data to DB

--- src/stock_detail/stock_detail.controller.ts
+++ src/stock_detail/stock_detail.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { StockDetailService } from './stock_detail.service';
+
 
 @Controller('stock-detail')
-export class StockDetailController {}
+export class StockDetailController {
+    constructor(private readonly stockDetailService: StockDetailService) {}
+
+    @Get()
+    readUser(){
+        console.log('I am here')
+        return this.stockDetailService.migrateData();
+    }
+}
--- src/stock_detail/stock_detail.module.ts
+++ src/stock_detail/stock_detail.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MasterSchema } from './master.model';
 import { StockDetailController } from './stock_detail.controller';
+import { StockDetailSchema } from './stock_detail.model';
 import { StockDetailService } from './stock_detail.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{name:'companyMasterData',schema:MasterSchema}]),
+    MongooseModule.forFeature([{name:'stockDetail',schema:StockDetailSchema}])
+  ],
   controllers: [StockDetailController],
-  providers: [StockDetailService]
+  providers: [StockDetailService],
 })
 export class StockDetailModule {}
